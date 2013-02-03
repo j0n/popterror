c7c18e49b76e65dcda39144444617d0b8bbca58d
Jon Andersson <andersson.jon@gmail.com>
2013-02-03 16:15:27 +0100

    adding tune config files

---
+++ js/tune/game.js
@@ -0,0 +1,47 @@
+define(function () {
+  return {
+    'jaguar': {
+      speed: 90
+    },
+    'motorAway': {
+      speed : 220,
+      5: {
+        action: 'obsticle',
+        done: false
+      },
+      12: {
+        action: 'obsticle',
+        done: false
+      },
+      51: {
+        action : 'speed',
+        speed: 300
+      },
+
+      56 : {
+        action : 'speed',
+        speed: 220
+      },
+      94 : {
+        action : 'speed',
+        speed: 320
+      },
+      97 : {
+        action : 'speed',
+        speed: 340
+      },
+      100 : {
+        action : 'speed',
+        speed: 400
+      },
+      125 : {
+        action : 'speed',
+        speed: 100
+      },
+      126 : {
+        action : 'end',
+      }
+      
+    }
+  }
+});
---
+++ js/tune/obsticle.js
@@ -0,0 +1,38 @@
+define(function () {
+  return {
+    'jaguar': {
+      10 : {
+        action : 'spin',
+        remove: 14, 
+        done: false
+      },
+      15 : {
+        action : 'spin',
+        remove: 16, 
+        done: false
+      }
+    },
+    'motorAway': {
+      3 : {
+        action : 'spin',
+        remove: 7, 
+        done: false
+      },
+      10 : {
+        action : 'spin',
+        remove: 13, 
+        done: false
+      },
+      16 : {
+        action : 'leftSlide',
+        remove: 18, 
+        done: false
+      },
+      20 : {
+        action : 'rightSlide',
+        remove: 22, 
+        done: false
+      },
+    }
+  }
+});
