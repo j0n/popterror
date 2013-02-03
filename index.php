fb7452da919c73fbcc10c931fecc8bc94234926e
Jon Andersson <andersson.jon@gmail.com>
2013-02-03 16:16:48 +0100

    adding songs in index and start button

--- index.php
+++ index.php
@@ -10,12 +10,21 @@
   <div id="legacyBrowser">
     Du har ingen canvas :(
   </div>
-
-  <div id="content">
-    
-  </div>
-  <div id="start-content">
-  </div>
+  <a id="start" href="#">
+    <h2 id="motorAway">Start song: Motoraway</h2>
+    <p>or</p>
+    <h2 id="jaguar">Start song: Jaguar</h2>
+  </a>
+  <div id="content"> </div>
+  <div id="car"></div>
+  <audio id="song-jaguar">
+    <source src="media/jaguar.mp3" type="audio/mpeg">
+    <source src="media/jaguar.ogg" type="audio/ogg">
+  </audio>
+  <audio id="song-motorAway">
+    <source src="media/motorAway.mp3" type="audio/mpeg">
+    <source src="media/motorAway.ogg" type="audio/ogg">
+  </audio>
 </body>
 </html>
 
