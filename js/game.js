bbfdda91fd03f88fdaaa7ef5debbc728a4745c06
Jon <andersson.jon@gmail.com>
2013-10-04 17:25:32 +0200

    more background

--- js/game.js
+++ js/game.js
@@ -11,14 +11,18 @@
       var myGame = new Phaser.Game(this, 'game', w, h, init, create, update);
       function init() {
           myGame.loader.addImageFile('bg', 'assets/trad_bak.png');
+          myGame.loader.addImageFile('bg2', 'assets/trad_fram.png');
+          myGame.loader.addImageFile('vag', 'assets/vag_streck.png');
           myGame.loader.addImageFile('car', 'assets/bil.png');
           myGame.loader.load();
       }
       function create() {
           myGame.camera.setBounds(0, 0, myGame.stage.width, myGame.stage.height);
           speed = config.song.speed;
-          myGame.createScrollZone('bg').setSpeed(speed, 0);
-          car = myGame.createSprite(w/2, h-100, 'car');
+          myGame.createScrollZone('bg').setSpeed(-speed, 0);
+          myGame.createScrollZone('bg2').setSpeed(-speed*0.3, 0);
+          myGame.createScrollZone('vag').setSpeed(-speed, 0);
+          car = myGame.createSprite(w/2, h-200, 'car');
           car.maxVelocity.setTo(150, 150);
       }
       function update(){
